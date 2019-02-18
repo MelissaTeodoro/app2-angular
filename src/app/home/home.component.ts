af79eaff134201bd59be151f5a6a6afcbfdf2bba
Melissa de Miranda Teodoro Soares <melissa.soares@spread.com.br>
2019-02-18 14:26:49 -0300

    Alterando o componente home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../ofertas.service';
+import { Oferta } from '../shared/ofertas.model';
 
+//Metadados
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [ OfertasService ]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public ofertas: Oferta[]
+
+  //Criar a variável do objeto que vai receber o serviço
+  constructor(private ofertasService: OfertasService) { }
 
   ngOnInit() {
+    this.ofertas = this.ofertasService.getOfertas()
+    console.log(this.ofertas)
   }
 
 }
