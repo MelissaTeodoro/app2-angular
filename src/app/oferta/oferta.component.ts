6be11418769a81cdd05f8cf4c5d09a2bf6216cc1
Melissa de Miranda Teodoro Soares <melissa.soares@spread.com.br>
2019-04-16 15:24:31 -0300

    Criação do input de pesquisa no componente topo

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,10 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router'; //Interface
 import { OfertasService } from '../ofertas.service'
 import { Oferta } from '../shared/ofertas.model';
-import { Observable, observable } from 'rxjs';
-import { Observer } from 'rxjs/Observer';
-import 'rxjs/Rx'
 
 //npm install rxjs-compat
 
@@ -14,7 +11,10 @@ import 'rxjs/Rx'
   styleUrls: ['./oferta.component.css'],
   providers: [OfertasService]
 })
-export class OfertaComponent implements OnInit {
+export class OfertaComponent implements OnInit, OnDestroy {
+
+  //private tempoObservableSubscription: Subscription
+  //private meuObservableTesteSubscription: Subscription
 
   public oferta: Oferta
 
@@ -40,26 +40,10 @@ export class OfertaComponent implements OnInit {
       //Conclusão do processamento do observable
       () => console.log('Processamento foi classificado como concluído')
     )*/
-
-    let tempo = Observable.interval(2000)
-
-    tempo.subscribe((intervalo: number) => { console.log(intervalo) })
-
-    //Observable (observável)
-    let meuObservableTeste = Observable.create((observer: Observer<string>) => {
-      observer.next('Primeiro evento da stream')
-      observer.next('Segundo evento da stream')
-      //Finalizar com sucesso a stream de eventos
-      observer.complete()
-      //observer.error('Algum erro foi encontrado')
-    })
-
-    //Observable (observador)
-    meuObservableTeste.subscribe(
-      (resultado: any) => console.log(resultado),
-      (erro: string) => console.log(erro),
-      () => console.log('A stream de eventos foi finalizada')
-    )
   }
 
+  ngOnDestroy() {
+
+  }
 }
+
