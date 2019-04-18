6f95fa34dbba4e84826d2a5e4ee1ef072fdae042
Melissa de Miranda Teodoro Soares <melissa.soares@spread.com.br>
2019-04-18 10:00:07 -0300

    Mudança no tipo de retorno da resposta dentro de ofertas.service

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -1,4 +1,4 @@
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import { Injectable } from '@angular/core';
 import { Oferta } from './shared/ofertas.model';
 import { URL_API } from './app.api';
@@ -16,19 +16,19 @@ export class OfertasService {
         //requisição http que retorna uma promessa com Oferta[]
         return this.http.get(`${URL_API}/ofertas?destaque=true`) //Esse verbo vai retornar um observable, porém vou converter para promise no momento
             .toPromise()
-            .then((resposta:any) => resposta.json()) //retorna um objeto literal
+            .then((resposta: Response) => resposta.json()) //retorna um objeto literal
     }
 
     public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
         return this.http.get(`${URL_API}/ofertas?categoria=${categoria}`)
         .toPromise()
-        .then((resposta: any) => resposta.json())
+        .then((resposta: Response) => resposta.json())
     }
 
     public getOfertaId(id: number): Promise<Oferta> {
         return this.http.get(`${URL_API}/ofertas?id=${id}`)
         .toPromise()
-        .then((resposta: any) => {
+        .then((resposta: Response) => {
             //Método .shift() extrai a primeira posição do array
             return resposta.json().shift()
         })
@@ -37,7 +37,7 @@ export class OfertasService {
     public getComoUsarOfertaPorId(id: number): Promise<string> {
         return this.http.get(`${URL_API}/como-usar?id=${id}`)
         .toPromise()
-        .then((resposta: any) => {
+        .then((resposta: Response) => {
             return resposta.json()[0].descricao
         })
     }
@@ -45,7 +45,7 @@ export class OfertasService {
     public getOndeFicaOfertaPorId(id: number): Promise<string> {
         return this.http.get(`${URL_API}/onde-fica?id=${id}`)
         .toPromise()
-        .then((resposta: any) => {
+        .then((resposta: Response) => {
             return resposta.json()[0].descricao
         })
     }
@@ -53,6 +53,6 @@ export class OfertasService {
     public pesquisaOfertas(termo: string): Observable<Oferta[]> {
         return this.http.get(`${URL_API}/ofertas?descricao_oferta_like=${termo}`) //_like = Pesquisa por aproximação
         .retry(10) //Quantidade de tentativas após falhas
-        .map((resposta: any)=> resposta.json()) //Método que transforma os valores para cada evento, gerando um novo Observable
+        .map((resposta: Response)=> resposta.json()) //Método que transforma os valores para cada evento, gerando um novo Observable
     }
 }
